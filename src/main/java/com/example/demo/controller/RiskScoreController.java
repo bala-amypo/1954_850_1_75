b9601b73f373160d993ad1f774e0aeec2ea2c761
bala-amypo <bala@amypo.in>
2025-12-22 04:13:27 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/RiskScoreController.java
@@ -0,0 +1,33 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RiskScore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Visitor visitor;
+
+    private Integer totalScore;
+
+    private String riskLevel;
+    // LOW / MEDIUM / HIGH / CRITICAL
+
+    private LocalDateTime evaluatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.evaluatedAt = LocalDateTime.now();
+    }
+}
