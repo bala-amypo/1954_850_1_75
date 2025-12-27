4f21eb1149bea57995a4787a273bf50cb40d8873
bala-amypo <bala@amypo.in>
2025-12-27 05:56:59 +0000

    User commit

--- src/main/java/com/example/demo/repository/RiskRuleRepository.java
+++ src/main/java/com/example/demo/repository/RiskRuleRepository.java
@@ -1,7 +1,10 @@
+
 package com.example.demo.repository;
 
-import com.example.demo.model.RiskRule;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.*;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.*;
 
+@Repository
 public interface RiskRuleRepository extends JpaRepository<RiskRule, Long> {
 }
